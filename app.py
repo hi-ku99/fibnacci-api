0f1e40b68fa948bbfd1365e74ead5c737b2eea7a
hirotake99 <takekusa426@gmail.com>
2023-03-13 16:41:33 +0900

    fib_ver1

--- app.py
+++ app.py
@@ -1,44 +1,44 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-  return "<h1>Hello, Flask!</h1>"
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=80, debug=True)
-
+from flask import Flask,request
 # app = Flask(__name__)
 
-# @app.route('/fib')
-# def get_request():
+# @app.route("/")
+# def index():
+#   return "<h1>Hello, Flask!</h1>"
 
-#     n = request.args.get('n', '')
-#     a, b = 0, 1
-#     fib_l=[]
+# if __name__ == "__main__":
+#   app.run(host="0.0.0.0", port=80, debug=True)
+
+app = Flask(__name__)
+
+@app.route('/fib')
+def get_request():
+
+    n = request.args.get('n', '')
+    a, b = 0, 1
+    fib_l=[]
     
-#     while n:
-#          n-=1
-#          fib_l.append(b)
-#          a, b = b, a+b
+    while n:
+         n-=1
+         fib_l.append(b)
+         a, b = b, a+b
     
-#     fib_sum = sum(fib_l)
+    fib_sum = sum(fib_l)
 
-#     body = { "results" : fib_sum}
+    body = { "results" : fib_sum}
     
-#     return body
+    return body
 
-# app.run()
+app.run()
 
-# @app.errorhandler(BadRequest)
-# @app.errorhandler(NotFound)
-# @app.errorhandler(InternalServerError)
-# def error_handler(e):
-#     res = jsonify({   
-#                     "status": error.name, 
-#                     "message": error.name 
-#                    })
-#     return res, e.code
+@app.errorhandler(BadRequest)
+@app.errorhandler(NotFound)
+@app.errorhandler(InternalServerError)
+def error_handler(e):
+    res = jsonify({   
+                    "status": error.name, 
+                    "message": error.name 
+                   })
+    return res, e.code
 
 
 
